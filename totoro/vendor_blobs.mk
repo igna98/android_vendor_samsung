ec5fa8621eeb04964e68dff056d043ae36e875bd
Rebel <gahlotpercy@gmail.com>
2013-07-31 20:18:18 +0530

    Update vendor_blobs.mk

--- totoro/vendor_blobs.mk
+++ totoro/vendor_blobs.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
+
 # Camera
 PRODUCT_COPY_FILES += \
     vendor/samsung/totoro/proprietary/system/lib/libarccamera.so:system/lib/libarccamera.so \
