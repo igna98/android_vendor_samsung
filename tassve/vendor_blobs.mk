50fa8b52ec057b2f5c5c33d36bd3211c77e9f87c
Robin <robin@Pavilion-g7.(none)>
2013-09-28 17:43:43 +0200

    add proper Tassve files

--- tassve/vendor_blobs.mk
+++ tassve/vendor_blobs.mk
@@ -12,18 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
+
 # Camera
 PRODUCT_COPY_FILES += \
-    vendor/samsung/cooperve/proprietary/lib/libarccamera.so:system/lib/libarccamera.so \
-    vendor/samsung/cooperve/proprietary/lib/libbrcmjpeg.so:system/lib/libbrcmjpeg.so \
-    vendor/samsung/cooperve/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-    vendor/samsung/cooperve/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-    vendor/samsung/cooperve/proprietary/lib/libCaMotion.so:system/lib/libCaMotion.so \
-    vendor/samsung/cooperve/proprietary/lib/libPanoraMax1.so:system/lib/libPanoraMax1.so \
-    vendor/samsung/cooperve/proprietary/lib/libseccamera.so:system/lib/libseccamera.so \
-    vendor/samsung/cooperve/proprietary/lib/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
-    vendor/samsung/cooperve/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
-    vendor/samsung/cooperve/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
-    vendor/samsung/cooperve/proprietary/lib/libcaps.so:system/lib/libcaps.so
+    vendor/samsung/tassve/proprietary/system/lib/libarccamera.so:system/lib/libarccamera.so \
+    vendor/samsung/tassve/proprietary/system/lib/libbrcmjpeg.so:system/lib/libbrcmjpeg.so \
+    vendor/samsung/tassve/proprietary/system/lib/libcamera.so:system/lib/libcamera.so \
+    vendor/samsung/tassve/proprietary/system/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/samsung/tassve/proprietary/system/lib/libCaMotion.so:system/lib/libCaMotion.so \
+    vendor/samsung/tassve/proprietary/system/lib/libPanoraMax1.so:system/lib/libPanoraMax1.so \
+    vendor/samsung/tassve/proprietary/system/lib/libseccamera.so:system/lib/libseccamera.so \
+    vendor/samsung/tassve/proprietary/system/lib/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
+    vendor/samsung/tassve/proprietary/system/lib/libcamera_client.so:system/lib/libcamera_client.so \
+    vendor/samsung/tassve/proprietary/system/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    vendor/samsung/tassve/proprietary/system/lib/libcaps.so:system/lib/libcaps.so \
+    vendor/samsung/cooperve/proprietary/lib/libmmcamera_statsproc30.so:system/lib/libmmcamera_statsproc30.so \
+    vendor/samsung/cooperve/proprietary/lib/libmmcamera_frameproc.so:system/lib/libmmcamera_frameproc.so \
+    vendor/samsung/cooperve/proprietary/lib/libmmcamera_faceproc.so:system/lib/libmmcamera_faceproc.so
 
-$(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
+# Offline 320 x 240 charging
+PRODUCT_COPY_FILES += \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_5.qmg:system/media/battery_charging_5.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_10.qmg:system/media/battery_charging_10.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_15.qmg:system/media/battery_charging_15.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_20.qmg:system/media/battery_charging_20.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_25.qmg:system/media/battery_charging_25.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_30.qmg:system/media/battery_charging_30.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_35.qmg:system/media/battery_charging_35.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_40.qmg:system/media/battery_charging_40.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_45.qmg:system/media/battery_charging_45.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_50.qmg:system/media/battery_charging_50.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_55.qmg:system/media/battery_charging_55.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_60.qmg:system/media/battery_charging_60.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_65.qmg:system/media/battery_charging_65.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_70.qmg:system/media/battery_charging_70.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_75.qmg:system/media/battery_charging_75.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_80.qmg:system/media/battery_charging_80.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_85.qmg:system/media/battery_charging_85.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_90.qmg:system/media/battery_charging_90.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_95.qmg:system/media/battery_charging_95.qmg \
+    vendor/samsung/tassve/proprietary/system/media/battery_charging_100.qmg:system/media/battery_charging_100.qmg \
+    vendor/samsung/tassve/proprietary/system/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
+    vendor/samsung/tassve/proprietary/system/media/Disconnected.qmg:system/media/Disconnected.qmg \
+    vendor/samsung/tassve/proprietary/system/media/TemperatureError.qmg:system/media/TemperatureError.qmg \
+    vendor/samsung/tassve/proprietary/system/bin/charging_mode:system/bin/charging_mode \
+    vendor/samsung/tassve/proprietary/system/bin/playlpm:system/bin/playlpm \
+    vendor/samsung/tassve/proprietary/system/lib/libqmage_bluesea.so:system/lib/libqmage_bluesea.so \
+    vendor/samsung/tassve/proprietary/system/lib/libQmageDecoder.so:system/lib/libQmageDecoder.so
