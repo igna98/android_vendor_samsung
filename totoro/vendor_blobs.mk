d6e391c662fa9db6790e4b7a1cf4d26c837f5e97
bieltv3 <bieltv.3@gmail.com>
2014-02-16 22:48:01 +0100

    Fix camera and offline charging totoro, first try of tassve

--- totoro/vendor_blobs.mk
+++ totoro/vendor_blobs.mk
@@ -27,3 +27,33 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/totoro/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
     vendor/samsung/totoro/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
     vendor/samsung/totoro/proprietary/lib/libcaps.so:system/lib/libcaps.so
+
+# Offline charging
+PRODUCT_COPY_FILES += \
+    vendor/samsung/totoro/proprietary/media/battery_charging_5.qmg:system/media/battery_charging_5.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_10.qmg:system/media/battery_charging_10.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_15.qmg:system/media/battery_charging_15.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_20.qmg:system/media/battery_charging_20.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_25.qmg:system/media/battery_charging_25.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_30.qmg:system/media/battery_charging_30.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_35.qmg:system/media/battery_charging_35.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_40.qmg:system/media/battery_charging_40.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_45.qmg:system/media/battery_charging_45.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_50.qmg:system/media/battery_charging_50.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_55.qmg:system/media/battery_charging_55.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_60.qmg:system/media/battery_charging_60.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_65.qmg:system/media/battery_charging_65.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_70.qmg:system/media/battery_charging_70.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_75.qmg:system/media/battery_charging_75.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_80.qmg:system/media/battery_charging_80.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_85.qmg:system/media/battery_charging_85.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_90.qmg:system/media/battery_charging_90.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_95.qmg:system/media/battery_charging_95.qmg \
+    vendor/samsung/totoro/proprietary/media/battery_charging_100.qmg:system/media/battery_charging_100.qmg \
+    vendor/samsung/totoro/proprietary/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
+    vendor/samsung/totoro/proprietary/media/Disconnected.qmg:system/media/Disconnected.qmg \
+    vendor/samsung/totoro/proprietary/media/TemperatureError.qmg:system/media/TemperatureError.qmg \
+    vendor/samsung/totoro/proprietary/bin/charging_mode:system/bin/charging_mode \
+    vendor/samsung/totoro/proprietary/bin/playlpm:system/bin/playlpm \
+    vendor/samsung/totoro/proprietary/lib/libqmage_bluesea.so:system/lib/libqmage_bluesea.so \
+    vendor/samsung/totoro/proprietary/lib/libQmageDecoder.so:system/lib/libQmageDecoder.so
