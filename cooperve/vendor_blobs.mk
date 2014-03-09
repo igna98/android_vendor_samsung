135da469ee07922b53f2f83794e02c8f193f32b3
bieltv3 <bieltv.3@gmail.com>
2014-03-09 17:51:19 +0100

    Upss, fix call

--- cooperve/vendor_blobs.mk
+++ cooperve/vendor_blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/samsung/cooperve/vendor.mk)
+$(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
 
 # Camera
 PRODUCT_COPY_FILES += \
