a493d20da237159e6cc62a80699fb3aab1beff06
igna98 <igna.98@outlook.com>
2014-07-20 09:05:01 -0300

    Update vendor_blobs.mk

--- totoro/vendor_blobs.mk
+++ totoro/vendor_blobs.mk
@@ -16,44 +16,44 @@ $(call inherit-product, vendor/samsung/bcm21553-common/vendor.mk)
 
 # Camera
 PRODUCT_COPY_FILES += \
-    vendor/samsung/totoro/proprietary/lib/libarccamera.so:system/lib/libarccamera.so \
-    vendor/samsung/totoro/proprietary/lib/libbrcmjpeg.so:system/lib/libbrcmjpeg.so \
-    vendor/samsung/totoro/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-    vendor/samsung/totoro/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-    vendor/samsung/totoro/proprietary/lib/libCaMotion.so:system/lib/libCaMotion.so \
-    vendor/samsung/totoro/proprietary/lib/libPanoraMax1.so:system/lib/libPanoraMax1.so \
-    vendor/samsung/totoro/proprietary/lib/libseccamera.so:system/lib/libseccamera.so \
-    vendor/samsung/totoro/proprietary/lib/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
-    vendor/samsung/totoro/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
-    vendor/samsung/totoro/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
-    vendor/samsung/totoro/proprietary/lib/libcaps.so:system/lib/libcaps.so
+    vendor/samsung/luisa/proprietary/lib/libarccamera.so:system/lib/libarccamera.so \
+    vendor/samsung/luisa/proprietary/lib/libbrcmjpeg.so:system/lib/libbrcmjpeg.so \
+    vendor/samsung/luisa/proprietary/lib/libcamera.so:system/lib/libcamera.so \
+    vendor/samsung/luisa/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
+    vendor/samsung/luisa/proprietary/lib/libCaMotion.so:system/lib/libCaMotion.so \
+    vendor/samsung/luisa/proprietary/lib/libPanoraMax1.so:system/lib/libPanoraMax1.so \
+    vendor/samsung/luisa/proprietary/lib/libseccamera.so:system/lib/libseccamera.so \
+    vendor/samsung/luisa/proprietary/lib/libseccameraadaptor.so:system/lib/libseccameraadaptor.so \
+    vendor/samsung/luisa/proprietary/lib/libcamera_client.so:system/lib/libcamera_client.so \
+    vendor/samsung/luisa/proprietary/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    vendor/samsung/luisa/proprietary/lib/libcaps.so:system/lib/libcaps.so
 
 # Device sensores
 PRODUCT_COPY_FILES += \
-    vendor/samsung/totoro/proprietary/lib/hw/sensors.totoro.so:system/lib/hw/sensors.totoro.so
+    vendor/samsung/luisa/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so
 
 # Offline charging
 PRODUCT_COPY_FILES += \
-    vendor/samsung/totoro/proprietary/media/battery_charging_5.qmg:system/media/battery_charging_5.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_10.qmg:system/media/battery_charging_10.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_15.qmg:system/media/battery_charging_15.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_20.qmg:system/media/battery_charging_20.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_25.qmg:system/media/battery_charging_25.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_30.qmg:system/media/battery_charging_30.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_35.qmg:system/media/battery_charging_35.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_40.qmg:system/media/battery_charging_40.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_45.qmg:system/media/battery_charging_45.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_50.qmg:system/media/battery_charging_50.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_55.qmg:system/media/battery_charging_55.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_60.qmg:system/media/battery_charging_60.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_65.qmg:system/media/battery_charging_65.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_70.qmg:system/media/battery_charging_70.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_75.qmg:system/media/battery_charging_75.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_80.qmg:system/media/battery_charging_80.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_85.qmg:system/media/battery_charging_85.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_90.qmg:system/media/battery_charging_90.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_95.qmg:system/media/battery_charging_95.qmg \
-    vendor/samsung/totoro/proprietary/media/battery_charging_100.qmg:system/media/battery_charging_100.qmg \
-    vendor/samsung/totoro/proprietary/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
-    vendor/samsung/totoro/proprietary/media/Disconnected.qmg:system/media/Disconnected.qmg \
-    vendor/samsung/totoro/proprietary/media/TemperatureError.qmg:system/media/TemperatureError.qmg
+    vendor/samsung/luisa/proprietary/media/battery_charging_5.qmg:system/media/battery_charging_5.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_10.qmg:system/media/battery_charging_10.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_15.qmg:system/media/battery_charging_15.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_20.qmg:system/media/battery_charging_20.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_25.qmg:system/media/battery_charging_25.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_30.qmg:system/media/battery_charging_30.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_35.qmg:system/media/battery_charging_35.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_40.qmg:system/media/battery_charging_40.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_45.qmg:system/media/battery_charging_45.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_50.qmg:system/media/battery_charging_50.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_55.qmg:system/media/battery_charging_55.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_60.qmg:system/media/battery_charging_60.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_65.qmg:system/media/battery_charging_65.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_70.qmg:system/media/battery_charging_70.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_75.qmg:system/media/battery_charging_75.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_80.qmg:system/media/battery_charging_80.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_85.qmg:system/media/battery_charging_85.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_90.qmg:system/media/battery_charging_90.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_95.qmg:system/media/battery_charging_95.qmg \
+    vendor/samsung/luisa/proprietary/media/battery_charging_100.qmg:system/media/battery_charging_100.qmg \
+    vendor/samsung/luisa/proprietary/media/chargingwarning.qmg:system/media/chargingwarning.qmg \
+    vendor/samsung/luisa/proprietary/media/Disconnected.qmg:system/media/Disconnected.qmg \
+    vendor/samsung/luisa/proprietary/media/TemperatureError.qmg:system/media/TemperatureError.qmg
